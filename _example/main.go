1cbdf9899b561499df90841326485b12c138ea71
Jakob Andersson <erikjakobandersson@gmail.com>
2024-03-08 08:20:08 +0100

    Update example code to use Write/Read wrappers

--- _example/main.go
+++ _example/main.go
@@ -29,25 +29,36 @@ func main() {
 
 	// 4. Send a message and wait for the response
 	// This triggers the first message round trip timer
-    // 4.a. Send a custom message and wait for the response
-    msg := "Hello, WebSocket!"
+    // 4.a. Write and read a message
+	msg := "Hello, WebSocket!"
+	startTime, err := ws.WriteMessage(websocket.TextMessage, []byte(msg))
+	if err != nil {
+        log.Fatalf("Failed to write message: %v", err)
+    }
+	msgType, p, err := ws.ReadMessage(startTime)
+	if err != nil {
+		log.Fatalf("Failed to read message: %v", err)
+	}
+	fmt.Printf("Received message '%s' of type '%d'\n\n", p, msgType)
+	// 4.b. Send a custom message and wait for the response in a single function
+    /* msg := "Hello, WebSocket!"
 	p, err := ws.SendMessage(websocket.TextMessage, []byte(msg))
     if err != nil {
         log.Fatalf("Failed to send message: %v", err)
     }
-	fmt.Printf("Received message: %s\n\n", p)
-	// 4.b. Send a JSON message and wait for the response
-	/* var msg = map[string]interface{}{"json": "message", "compatible": "with", "your": "backend"}
+	fmt.Printf("Received message: %s\n\n", p) */
+	// 4.c. Send a JSON message and wait for the response
+	/* var msg = map[string]interface{}{"json": "message", "compatible": "with", "your": "target", "ws": "server"}
 	p, err := ws.SendMessageJSON(msg)
     if err != nil {
         log.Fatalf("Failed to send message: %v", err)
     }
 	log.Printf("Received message: %s", p) */
-	// 4.c. If not interested in the response, you can use the basic message sending method
+	// 4.d. If not interested in the response, you can use the basic message sending method
 	/* if err := ws.SendMessageBasic(); err != nil {
 		log.Fatalf("Failed to send message: %v", err)
 	} */
-	// 4.d. Alternatively just send a ping message
+	// 4.e. Alternatively just send a ping message
 	/* if err := ws.SendPing(); err != nil {
 		log.Fatalf("Failed to send ping: %v", err)
 	} */
